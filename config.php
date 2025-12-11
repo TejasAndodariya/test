0343039ceb35a5943ec236d4b6e337f6ae0f6509
Tejas Andodariya <tejasandodariya@gmail.com>
2025-12-11 23:24:45 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $host = "jdbc:mysql://sql12.freesqldatabase.com:3306/";  // Your MySQL hostname
 $user = "sql12811562";            // Your DB username
 $pass = "VAaQzQMVBy";            // Your DB password
-$db   = "sql12811562_studentdb";  // Your database name
+$db   = "sql12811562";  // Your database name
 
 $conn = new mysqli($host, $user, $pass, $db);
 
@@ -10,3 +10,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
